ec1f3d416b6f89a2f81fb14fcd7912c77feffa9c
Anthony_256 <byansianthony@gmail.com>
2023-03-04 10:10:34 +0000

    random fores

---
+++ predictive-healthcare-analytics/models/random_forest.py
@@ -0,0 +1,39 @@
+"""
+random_forest.py
+
+This script implements a random forest model for predicting patient outcomes, such as mortality or disease progression,
+based on various factors such as demographics, comorbidities, and treatments.
+
+"""
+
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+
+# Load the preprocessed data
+data = pd.read_csv('data/processed/cleaned_data.csv')
+
+# Split the data into training and testing sets
+X = data.drop('outcome', axis=1)
+y = data['outcome']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Fit a random forest model to the training data
+model = RandomForestClassifier(n_estimators=100, max_depth=10, random_state=42)
+model.fit(X_train, y_train)
+
+# Make predictions on the testing data
+y_pred = model.predict(X_test)
+
+# Evaluate the model performance
+accuracy = accuracy_score(y_test, y_pred)
+precision = precision_score(y_test, y_pred)
+recall = recall_score(y_test, y_pred)
+f1 = f1_score(y_test, y_pred)
+
+print('Model performance:')
+print('Accuracy: {:.2f}'.format(accuracy))
+print('Precision: {:.2f}'.format(precision))
+print('Recall: {:.2f}'.format(recall))
+print('F1-score: {:.2f}'.format(f1))
